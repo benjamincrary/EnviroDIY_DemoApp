7b1c46f141caf9cdab7fba7dc2e58aa64a13bcf4
benjamincrary <benjamin.crary@gmail.com>
2018-08-02 14:32:55 -0500

    Updated variable names + removed Battery/BoardTemp
    variable names updated in response to changes at data.enviro.diy
    
    battery and board temp removed due to issues with web service connections. will flag issues for WaterML package, USU, and/or Emilio

--- Crosslands/CacheUpdate.R
+++ Crosslands/CacheUpdate.R
@@ -4,20 +4,26 @@
 ### Comments: Server errors/timeouts may require breaking up GetValues functions into severl iterations and binding the results together (see air temperature)
 ###
 ### 09/12/2017
+### Updated: 11/14/2017 for new endpoints
 
 
 library(WaterML)
 library(tidyverse)
 
 
-server <- "http://odm2wofpy.uwrl.usu.edu:8080/odm2timeseries/soap/cuahsi_1_0/.wsdl"
+#server <- "http://data.envirodiy.org/wofpy/soap/cuahsi_1_1/.wsdl"
+server <- "http://data.envirodiy.org/wofpy/rest_1_1/"
+server <- "http://hiscentral.cuahsi.org/pub_network.aspx?n=5644?WSDL"
+server <- ""
+sites <- GetSites(server)
+sites <- GetSites("http://data.envirodiy.org/wofpy/rest/1_1/GetSites")
 
 ## water depth
-wdepth <- GetValues(server, siteCode="odm2timeseries:160065_Crosslands", variableCode="odm2timeseries:MaxBotix_MB7386_Distance")
-tidy <- separate(wdepth, time, into=c("date", "time"), sep="T")
+wdepth <- GetValues(server, siteCode="envirodiy:160065_Limno_Crossroads", variableCode="envirodiy:MaxBotix_MB7386_Distance")
+tidy <- separate(wdepth, time, into=c("date", "time"), sep=" ")
 tidy$datetime <- paste(tidy$date, " ", tidy$time,sep="")
-tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S"))-11*60*60
-tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d"))-11*60*60
+tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S",tz="America/Chicago"))-11*60*60
+tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d", tz="America/Chicago"))-11*60*60
 wdepth$datetime <- tidy$datetime
 wdepth$date <- tidy$date
 wdepth <- wdepth %>% filter(DataValue != 0)
@@ -26,77 +32,78 @@ wdepth <- wdepth %>% filter(DataValue < 4000)
 wdepth <- wdepth %>% filter(DataValue > 1000)
 wdepth$Depth = (4000 - wdepth$DataValue)/1000
 
-saveRDS(wdepth, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/cache_wdepth.RDS")
+saveRDS(wdepth, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/Crosslands/cache_wdepth.RDS")
 #wdepthcache <- readRDS("C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/cache_wdepth.RDS")
 
 
 ## water temp
-wtemp <- GetValues(server, siteCode="odm2timeseries:160065_Crosslands", variableCode="odm2timeseries:Adafruit_DS18B20_Temp")
-tidy <- separate(wtemp, time, into=c("date", "time"), sep="T")
+wtemp <- GetValues(server, siteCode="envirodiy:160065_Limno_Crossroads", variableCode="envirodiy:Maxim_DS18B20_Temp")
+tidy <- separate(wtemp, time, into=c("date", "time"), sep=" ")
 tidy$datetime <- paste(tidy$date, " ", tidy$time,sep="")
-tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S"))-11*60*60
-tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d"))-11*60*60
+tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S", tz="America/Chicago"))-11*60*60
+tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d",tz="America/Chicago"))-11*60*60
 wtemp$datetime <- tidy$datetime
 wtemp$date <- tidy$date
 wtemp$DataValue <- wtemp$DataValue*9/5+32
-saveRDS(wtemp, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/cache_wtemp.RDS")
+saveRDS(wtemp, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/Crosslands/cache_wtemp.RDS")
 
 
 ## air temp
+atemp <- GetValues(server, siteCode="envirodiy:160065_Limno_Crossroads", variableCode="envirodiy:Bosch_BME280_Temp")
+#atemp1 <- GetValues(server, siteCode="envirodiy:160065_Limno_Crossroads", variableCode="envirodiy:Seeed_BME280_Temp", startDate=Sys.Date()-170,endDate=Sys.Date()-140)
+#atemp2 <- GetValues(server, siteCode="envirodiy:160065_Limno_Crossroads", variableCode="envirodiy:Seeed_BME280_Temp", startDate = Sys.Date()-139, endDate=Sys.Date()-110)
+#atemp3 <- GetValues(server, siteCode="envirodiy:160065_Limno_Crossroads", variableCode="envirodiy:Seeed_BME280_Temp",startDate  = Sys.Date()-109, endDate=Sys.Date()-80)
+#atemp4 <- GetValues(server, siteCode="envirodiy:160065_Limno_Crossroads", variableCode="envirodiy:Seeed_BME280_Temp",startDate  = Sys.Date()-79, endDate=Sys.Date()-50)
+#atemp5 <- GetValues(server, siteCode="envirodiy:160065_Limno_Crossroads", variableCode="envirodiy:Seeed_BME280_Temp",startDate  = Sys.Date()-49, endDate=Sys.Date()+2)
+#atemp <- rbind(atemp1,atemp2, atemp3, atemp4, atemp5)
 
-atemp1 <- GetValues(server, siteCode="odm2timeseries:160065_Crosslands", variableCode="odm2timeseries:Seeed_BME280_Temp", startDate=Sys.Date()-170,endDate=Sys.Date()-140)
-atemp2 <- GetValues(server, siteCode="odm2timeseries:160065_Crosslands", variableCode="odm2timeseries:Seeed_BME280_Temp", startDate = Sys.Date()-139, endDate=Sys.Date()-110)
-atemp3 <- GetValues(server, siteCode="odm2timeseries:160065_Crosslands", variableCode="odm2timeseries:Seeed_BME280_Temp",startDate  = Sys.Date()-109, endDate=Sys.Date()-80)
-atemp4 <- GetValues(server, siteCode="odm2timeseries:160065_Crosslands", variableCode="odm2timeseries:Seeed_BME280_Temp",startDate  = Sys.Date()-79, endDate=Sys.Date()-50)
-atemp5 <- GetValues(server, siteCode="odm2timeseries:160065_Crosslands", variableCode="odm2timeseries:Seeed_BME280_Temp",startDate  = Sys.Date()-49, endDate=Sys.Date()+2)
-atemp <- rbind(atemp1,atemp2, atemp3, atemp4, atemp5)
-tidy <- separate(atemp, time, into=c("date", "time"), sep="T")
+tidy <- separate(atemp, time, into=c("date", "time"), sep=" ")
 tidy$datetime <- paste(tidy$date, " ", tidy$time,sep="")
-tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S"))-11*60*60
-tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d"))-11*60*60
+tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S", tz="America/Chicago"))-11*60*60
+tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d", tz="America/Chicago"))-11*60*60
 atemp$datetime <- tidy$datetime
 atemp$date <- tidy$date
 atemp$DataValue <- atemp$DataValue*9/5+32
 atemp <- filter(atemp, DataValue < 120)
-saveRDS(atemp, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/cache_atemp.RDS")
+saveRDS(atemp, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/Crosslands/cache_atemp.RDS")
 
 
 ## humidity
-humidity <- GetValues(server, siteCode="odm2timeseries:160065_Crosslands", variableCode="odm2timeseries:Seeed_BME280_humidity")
-tidy <- separate(humidity, time, into=c("date", "time"), sep="T")
+humidity <- GetValues(server, siteCode="envirodiy:160065_Limno_Crossroads", variableCode="envirodiy:Bosch_BME280_Humidity")
+tidy <- separate(humidity, time, into=c("date", "time"), sep=" ")
 tidy$datetime <- paste(tidy$date, " ", tidy$time,sep="")
-tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S"))-11*60*60
-tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d"))-11*60*60
+tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S", tz="America/Chicago"))-11*60*60
+tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d", tz="America/Chicago"))-11*60*60
 humidity$datetime <- tidy$datetime
 humidity$date <- tidy$date
-saveRDS(humidity, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/cache_humidity.RDS")
+saveRDS(humidity, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/Crosslands/cache_humidity.RDS")
 
 
 
 ## Battery
 
-bbatt <- GetValues(server, siteCode="odm2timeseries:160065_Crosslands", variableCode="odm2timeseries:EnviroDIY_Mayfly_Volt")
-tidy <- separate(bbatt, time, into=c("date", "time"), sep="T")
+bbatt <- GetValues(server, siteCode="envirodiy:160065_Limno_Crossroads", variableCode="envirodiy:EnviroDIY_Mayfly_Batt")
+tidy <- separate(bbatt, time, into=c("date", "time"), sep=" ")
 tidy$datetime <- paste(tidy$date, " ", tidy$time,sep="")
-tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S"))-11*60*60
-tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d"))-11*60*60
+tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S", tz="America/Chicago"))-11*60*60
+tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d", tz="America/Chicago"))-11*60*60
 bbatt$datetime <- tidy$datetime
 bbatt$date <- tidy$date
-saveRDS(bbatt, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/cache_bbatt.RDS")
+saveRDS(bbatt, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/Crosslands/cache_bbatt.RDS")
 
 
 ## board temp
 
 
-btemp <- GetValues(server, siteCode="odm2timeseries:160065_Crosslands", variableCode="odm2timeseries:EnviroDIY_Mayfly_Temp")
-tidy <- separate(btemp, time, into=c("date", "time"), sep="T")
+btemp <- GetValues(server, siteCode="envirodiy:160065_Limno_Crossroads", variableCode="envirodiy:EnviroDIY_Mayfly_Temp")
+tidy <- separate(btemp, time, into=c("date", "time"), sep=" ")
 tidy$datetime <- paste(tidy$date, " ", tidy$time,sep="")
-tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S"))-11*60*60
-tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d"))-11*60*60
+tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S", tz="America/Chicago"))-11*60*60
+tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d", tz="America/Chicago"))-11*60*60
 btemp$datetime <- tidy$datetime
 btemp$date <- tidy$date
 btemp$DataValue <- btemp$DataValue*9/5+32
-saveRDS(btemp, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/cache_btemp.RDS")
+saveRDS(btemp, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/Crosslands/cache_btemp.RDS")
 
 
 
@@ -104,14 +111,14 @@ saveRDS(btemp, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/cache_b
 
 ## free ram
 
-bram <- GetValues(server, siteCode="odm2timeseries:160065_Crosslands", variableCode="odm2timeseries:EnviroDIY_Mayfly_FreeSRAM")
-tidy <- separate(bram, time, into=c("date", "time"), sep="T")
+bram <- GetValues(server, siteCode="envirodiy:160065_Limno_Crossroads", variableCode="envirodiy:EnviroDIY_Mayfly_FreeRAM")
+tidy <- separate(bram, time, into=c("date", "time"), sep=" ")
 tidy$datetime <- paste(tidy$date, " ", tidy$time,sep="")
-tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S"))-11*60*60
-tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d"))-11*60*60
+tidy$datetime <- as.POSIXct(strptime(tidy$datetime,format="%Y-%m-%d %H:%M:%S", tz="America/Chicago"))-11*60*60
+tidy$date <- as.POSIXct(strptime(tidy$date, format="%Y-%m-%d", tz="America/Chicago"))-11*60*60
 bram$datetime <- tidy$datetime
 bram$date <- tidy$date
-saveRDS(bram, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/cache_bram.RDS")
+saveRDS(bram, "C:/Users/bcrary/Desktop/Projects/BigCZ/EnviroDIY_DemoApp/Crosslands/cache_bram.RDS")
 
 
 
